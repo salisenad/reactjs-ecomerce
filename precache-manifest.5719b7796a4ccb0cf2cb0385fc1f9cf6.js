7a790fd19ae0fa9a7cc4454a1c76b1191d42c992
Senad <senad.sali3@gmail.com>
2020-03-29 14:30:07 -0700

    Updates

---
+++ precache-manifest.5719b7796a4ccb0cf2cb0385fc1f9cf6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d02eb26ee75907fc94fd6cd5288744ec",
+    "url": "/reactjs-ecomerce/index.html"
+  },
+  {
+    "revision": "5382cd63e593725d9312",
+    "url": "/reactjs-ecomerce/static/css/main.d5781a99.chunk.css"
+  },
+  {
+    "revision": "9b67a9d27faa4fc97dc2",
+    "url": "/reactjs-ecomerce/static/js/2.2bc0bdec.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/reactjs-ecomerce/static/js/2.2bc0bdec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5382cd63e593725d9312",
+    "url": "/reactjs-ecomerce/static/js/main.c79a7ce0.chunk.js"
+  },
+  {
+    "revision": "35cc74289440cfc337c3",
+    "url": "/reactjs-ecomerce/static/js/runtime-main.cdb00a0a.js"
+  }
+]);
